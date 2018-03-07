03a18a74caf11305cb698d485ebd3b44b8556e35
Kyle Patterson <kdpatters@gmail.com>
2018-03-07 01:25:02 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# -----------------------------------------------------------------------------------------
+#                                Extraterrestrial colony simulator
+# -----------------------------------------------------------------------------------------
+#                           Created by Kyle D. Patterson and Julia Holgado
+#                                            Spring 2018
+#
+
+from ecs import *
+
+if __name__ == "__main__":
+    # Process commandline arguments
+    # Should we load a game a create a new one?
+    if load_game:
+        # load(<save file>)
+    else:
+        # create_new_game()
+
+    # Game loop
+    while True:
+        # Generate next event
+        # generate_event()
+        
+        # Listen for end turn event
+
+        # Take step
+        # step()
